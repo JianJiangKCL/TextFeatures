58f97e35d462bca9af1e3395b3312e312955da6a
JianJiang_Omen <jian.jiang@kcl.ac.uk>
2021-12-19 21:16:41 +0000

    gender bug fixed; chuanlian training

--- train_modality.py
+++ train_modality.py
@@ -7,7 +7,7 @@ import pickle
 import autokeras as ak
 import tensorflow as tf
 import pandas as pd
-
+import argparse
 #%%
 def df_drop_cols(df, cols):
     for col in cols:
@@ -32,6 +32,7 @@ def read_data_age(path):
     return data, Y, O
 
 def build_dataset(root, mode, seperator):
+    print('build dataset...')
     labels = ['OPENMINDEDNESS_Z', 'CONSCIENTIOUSNESS_Z', 'EXTRAVERSION_Z', 'AGREEABLENESS_Z', 'NEGATIVEEMOTIONALITY_Z']
 
     if seperator == 'age':
@@ -40,30 +41,30 @@ def build_dataset(root, mode, seperator):
         read_data = read_data_gender
     if mode == 'train':
         train_path = os.path.join(root, f'train_data.csv')
-        data, M, F = read_data(train_path)
+        data, M_data, F_data = read_data(train_path)
 
         val_path = os.path.join(root, f'validation_data.csv')
         val_data, val_M, val_F = read_data(val_path)
 
         data = data.append(val_data)
-        M = M.append(val_M)
-        F = F.append(val_F)
+        M_data = M_data.append(val_M)
+        F_data = F_data.append(val_F)
 
     else:
         path = os.path.join(root, f'{mode}_data.csv')
-        data, M, F = read_data(path)
+        data, M_data, F_data = read_data(path)
 
     # these are ground truth for each OCEAN; we will use them to evaluate the performance of the model.
     # [1,N]
-    def get_OCEAN(data):
+    def get_OCEAN(d):
         OCEAN = []
         for i in range(0, 5):
-            OCEAN.append(data.pop(labels[i]).to_numpy())
+            OCEAN.append(d.pop(labels[i]).to_numpy())
         return OCEAN
 
     comb_OCEAN = get_OCEAN(data)
-    M_OCEAN = get_OCEAN(M)
-    F_OCEAN = get_OCEAN(F)
+    M_OCEAN = get_OCEAN(M_data)
+    F_OCEAN = get_OCEAN(F_data)
 
 
     #%%
@@ -72,6 +73,8 @@ def build_dataset(root, mode, seperator):
 
     # combined
     comb = df_drop_cols(data, drop_cols)
+    M = df_drop_cols(M_data, drop_cols)
+    F = df_drop_cols(F_data, drop_cols)
 
     Fb_drop_cols = [(f'{i}_fb') for i in range(0, 552)]
     Bt_drop_cols = [(f'{i}_bt') for i in range(0, 512)]
@@ -101,84 +104,94 @@ def build_dataset(root, mode, seperator):
 
     return comb_dataset, M_dataset, F_dataset
 
-# comb_train_set, M_train_set, F_train_set = build_dataset('multi_modality_csv', 'train', 'gender')
-#
-# comb_val_set, M_val_set, F_val_set = build_dataset('multi_modality_csv', 'validation', 'gender')
+def main(args):
+    comb_train_set, M_train_set, F_train_set = build_dataset('multi_modality_csv', 'train', 'gender')
 
-comb_train_set, Y_train_set, O_train_set = build_dataset('multi_modality_csv', 'train', 'age')
+    comb_val_set, M_val_set, F_val_set = build_dataset('multi_modality_csv', 'validation', 'gender')
+    #
+    # comb_train_set, Y_train_set, O_train_set = build_dataset('multi_modality_csv', 'train', 'age')
+    #
+    # comb_val_set, Y_val_set, O_val_set = build_dataset('multi_modality_csv', 'validation', 'age')
 
-comb_val_set, Y_val_set, O_val_set = build_dataset('multi_modality_csv', 'validation', 'age')
+    # comb_test_set , M_test_set, F_test_set = build_dataset('multi_modality_csv', 'test')
 
-# comb_test_set , M_test_set, F_test_set = build_dataset('multi_modality_csv', 'test')
+    model_names = ['Model_O','Model_C','Model_E','Model_A','Model_N']
 
-model_names = ['Model_O','Model_C','Model_E','Model_A','Model_N']
+    #%%
+    dump = './Dump Data'
+    path = './Textual Models'
+    training_name = 'multiModality_outputs'
+    models_path = path+'/'+training_name
+    # models_path = path +'/' + 'multiModality_outputs'
+    combined_path = models_path+'/Combined'
+    age_path = models_path+'/Age'
+    gender_path = models_path+'/Gender'
+    male_path = models_path+'/Gender/Male'
+    female_path = models_path+'/Gender/Female'
+    old_path = models_path+'/Age/Old'
+    young_path = models_path+'/Age/Young'
 
-#%%
-dump = './Dump Data'
-path = './Textual Models'
-training_name = 'multiModality_outputs'
-models_path = path+'/'+training_name
-# models_path = path +'/' + 'multiModality_outputs'
-combined_path = models_path+'/Combined'
-age_path = models_path+'/Age'
-gender_path = models_path+'/Gender'
-male_path = models_path+'/Gender/Male'
-female_path = models_path+'/Gender/Female'
-old_path = models_path+'/Age/Old'
-young_path = models_path+'/Age/Young'
+    folders = [ dump
+    ,path
+    ,models_path
+    ,combined_path
+    ,age_path
+    ,gender_path
+    ,male_path
+    ,female_path
+    ,old_path
+    ,young_path]
 
-folders = [ dump
-,path
-,models_path
-,combined_path
-,age_path
-,gender_path
-,male_path
-,female_path
-,old_path
-,young_path]
+    #%%
 
-#%%
+    for folder in folders:
+        # exist mkdir
+        if not os.path.exists(folder):
+            os.mkdir(folder)
 
-for folder in folders:
-    try:
-        os.mkdir(folder)
-    except OSError as error:
-        print(error)
+    def train_setting(setting, train_set, val_set, path):
+        print(f"{setting} TRAINING BEGINS")
+        # iter for 5 OCEAN dataset
+        for i in range(2, 5):
 
-def train_setting(setting, train_set, val_set, path):
-    print(f"{setting} TRAINING BEGINS")
-    # iter for 5 OCEAN dataset
-    for i in range(5):
+            train = train_set[i]
+            val = val_set[i]
+            print(model_names[i])
+            # Define a regressor
+            total_reg = ak.AutoModel(
+              # by default, RegrssionHead uses MSE
+              inputs=[ak.StructuredDataInput(), ak.StructuredDataInput()],
+              outputs=[ak.RegressionHead()],
+              max_trials=100, overwrite=True, project_name=setting+model_names[i], directory='./Dump Data')
 
-        train = train_set[i]
-        val = val_set[i]
-        print(model_names[i])
-        # Define a regressor
-        total_reg = ak.AutoModel(
-          # by default, RegrssionHead uses MSE
-          inputs=[ak.StructuredDataInput(), ak.StructuredDataInput()],
-          outputs=[ak.RegressionHead()],
-          max_trials=100, overwrite=True, project_name=setting+model_names[i], directory='./Dump Data')
+            # Feed the tensorflow Dataset to the regressor.
+            total_reg.fit([train[0], train[1]], [train[2]], epochs=1000, validation_split=0.15)
+            # Convert to model
+            total_model = total_reg.export_model()
+            # Evaluate on validation set
+            evaluation = total_reg.evaluate([val[0], val[1]], [val[2]])
+            # Write loss and error to a file
+            with open('./Dump Data/'+training_name + setting+'_eval_val.txt', 'a') as f:
+              f.write(training_name+setting+'_'+model_names[i]+' -> ')
+              f.write(str(evaluation))
+              f.write('\n')
+            # Save current model
+            total_model.save(path+'/'+model_names[i])
 
-        # Feed the tensorflow Dataset to the regressor.
-        total_reg.fit([train[0], train[1]], [train[2]], epochs=1000, validation_split=0.15)
-        # Convert to model
-        total_model = total_reg.export_model()
-        # Evaluate on validation set
-        evaluation = total_reg.evaluate([val[0], val[1]], [val[2]])
-        # Write loss and error to a file
-        with open('./Dump Data/'+training_name + setting+'_eval_val.txt', 'a') as f:
-          f.write(training_name+setting+'_'+model_names[i]+' -> ')
-          f.write(str(evaluation))
-          f.write('\n')
-        # Save current model
-        total_model.save(path+'/'+model_names[i])
-
-    print(f"{setting} training done!!")
+        print(f"{setting} training done!!")
 
 
-# train_setting('Female', F_train_set, F_val_set, female_path)
-# train_setting('Male', M_train_set, M_val_set, male_path)
-# train_setting('Female', F_train_set, F_val_set, female_path)
-# train_setting('Combined', comb_train_set, comb_val_set, combined_path)
+    # train_setting('Female', F_train_set, F_val_set, female_path)
+    # train_setting('Male', M_train_set, M_val_set, male_path)
+    # train_setting('Female', F_train_set, F_val_set, female_path)
+    print('start training..')
+    train_setting('Combined', comb_train_set, comb_val_set, combined_path)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--OCEAN_id", type=str)
+    # parser.add_argument("--src_dir", type=str, default="")
+    parser.add_argument("--setting", type=str)
+    args = parser.parse_args()
+    main(args)
